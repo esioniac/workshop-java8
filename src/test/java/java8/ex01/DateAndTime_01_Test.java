0234c194a51b40c550477a60bc0f4873a9c531cb
Manu <esioniac@gmail.com>
2018-07-31 15:56:45 +0200

    Fin du TP6 Date&Time

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -14,16 +14,23 @@ import static org.junit.Assert.assertThat;
 public class DateAndTime_01_Test {
 
     // Vous vous souvenez de java.util.Date (Java 1) ?
-    @Test
+	@Test
     public void test_date() {
 
         // TODO modifier les paramètres pour que le test soit passant
-        Date date = new Date(0, 0, 0, 0,0,0);
+		Date date = new Date(2017-1900, 2, 24, 1, 2,3);
 
         assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // une date est mutable... :-(
         // TODO modifier le jour de la date pour que le test soit passant
+		
+        date.setDate(25);
+        date.setMonth(2);
+        date.setHours(01);
+        date.setYear(2017-1900);
+        date.setMinutes(02);
+        date.setSeconds(03);
 
         assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
@@ -35,12 +42,13 @@ public class DateAndTime_01_Test {
         Calendar calendar = Calendar.getInstance();
 
         // TODO modifier l'objet calendar pour que le test soit passant
+        calendar.setTime(new Date(2017-1900, 2, 24, 1, 2,3));
 
         assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // calendar est mutable... :-(
         // TODO modifier l'objet calendar pour que le test soit passant
-
+        calendar.setTime(new Date(2017-1900, 2, 25, 1, 2,3));
         assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
